c1a1417394b31df53222dfc7c9ea8d1c1d5b48dd
Fabio Ramos <fabio.d.o.ramos@gmail.com>
2022-02-28 12:02:55 +1100

    remove count on resource

--- ssm.tf
+++ ssm.tf
@@ -1,5 +1,4 @@
 resource "aws_ssm_parameter" "mwaa_env" {
-  #   count       = var.secret_method == "ssm" ? 1 : 0
   name        = "/mwaa/ENV"
   description = "MWAA Environment"
   value       = var.environment_name
