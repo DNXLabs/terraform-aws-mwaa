d9679af57c6b4bde78d5cdb40b7e6a4fc7f41365
doug1988 <33627994+doug1988@users.noreply.github.com>
2025-03-20 13:05:45 +1100

    Remove route53

--- _variables.tf
+++ _variables.tf
@@ -62,12 +62,5 @@ variable "vpn_cidr" {
   description = "VPN CIDR Access for Airflow UI"
 }
 
-variable "custom_domain" {
-  type        = string
-  description = "custom DNS name for MWAA Environment"
-}
 
-variable "aws_route53_id" {
-  type        = string
-  description = "Route53 Hosted Zone ID"
-}
+
--- mwaa-route53.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route53_record" "mwaa" {
-
-  zone_id = var.aws_route53_id
-  name    = var.custom_domain
-  type    = "CNAME"
-  ttl     = "60"
-
-  records = [aws_mwaa_environment.mwaa.webserver_url]
-}
